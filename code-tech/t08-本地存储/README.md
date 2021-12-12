d85b258c82508b5c9c9db2d079efb09db5410b0c
SageKwun <sagekwun@gmail.com>
2021-12-12 10:56:23 +0800

    t08 本地存储

---
+++ code-tech/t08-本地存储/README.md
@@ -0,0 +1,21 @@
+# 上周作业
+
+# 本地存储
+
+## Cookie
+
+## SessionStorage
+
+## localStorage
+
+# 寒假及下学期安排
+
+## 寒假（录播、直播）
+
+前端工程化
+
+## 下学期
+
+Vue3 or React
+
+## 留任
---
+++ code-tech/t08-本地存储/index.js
@@ -0,0 +1,17 @@
+// Cookie
+
+document.cookie;
+
+// SessionStorage
+
+sessionStorage.setItem("name", "kk");
+console.log(sessionStorage.getItem("name"));
+console.log(sessionStorage.removeItem("name"));
+console.log(sessionStorage.getItem("name"));
+
+// localStorage
+
+localStorage.setItem("name", "kk");
+console.log(localStorage.getItem("name"));
+console.log(localStorage.removeItem("name"));
+console.log(localStorage.getItem("name"));
